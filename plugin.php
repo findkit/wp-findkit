5fae98499e3e28cda185f7bbde35858b04a03e86
Esa-Matti Suuronen <esa-matti@suuronen.org>
2023-06-05 20:23:41 +0300

    Fix Finkdit

--- plugin.php
+++ plugin.php
@@ -14,7 +14,7 @@
 // class_exists(). If not it means there's no global autoloader in place and
 // the user is not using composer. In that case we can safely require the
 // bundled autoloader code.
-if (!\class_exists('\Finkdit\Loader')) {
+if (!\class_exists('\Findkit\Loader')) {
 	require_once __DIR__ . '/vendor/autoload.php';
 }
 // This way we can add the vendor/ directory to git and have the plugin "just
