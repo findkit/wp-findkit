d457e09cd349898724e8e9d62d5238f91df8ecaf
Esa-Matti Suuronen <esa-matti@suuronen.org>
2023-09-19 11:32:43 +0300

    Add Findkit sidebar to Gutenberg

---
+++ build/Gutenberg/blocks/sidebar/sidebar.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-plugins'), 'version' => '01dfc065707a814a51b7');
---
+++ src/Gutenberg/FindkitBlocks.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Findkit\Gutenberg;
+
+use Findkit\Utils;
+
+class FindkitBlocks
+{
+	private $post_types;
+
+	function bind()
+	{
+		\add_action('init', [$this, '__action_init']);
+
+		\add_action('admin_enqueue_scripts', [
+			$this,
+			'__action_enqueue_block_editor_assets',
+		]);
+	}
+
+	function __action_enqueue_block_editor_assets()
+	{
+		\wp_localize_script(
+			'findkit-sidebar-editor-script',
+			'FINDKIT_GUTENBERG_SIDEBAR',
+			[
+				'postTypes' => $this->post_types,
+			]
+		);
+	}
+
+	function __action_init()
+	{
+		$this->post_types = \apply_filters('findkit_sidebar_post_types', [
+			'post',
+			'page',
+		]);
+
+		foreach ($this->post_types as $type) {
+			register_post_meta($type, '_findkit_superwords', [
+				'show_in_rest' => true,
+				'single' => true,
+				'type' => 'string',
+				'auth_callback' => function () {
+					return \current_user_can('edit_posts');
+				},
+			]);
+
+			register_post_meta($type, '_findkit_show_in_search', [
+				'show_in_rest' => true,
+				'single' => true,
+				'type' => 'string',
+				'auth_callback' => function () {
+					return \current_user_can('edit_posts');
+				},
+			]);
+		}
+
+		$success = register_block_type(
+			__DIR__ . '/../../build/Gutenberg/blocks/sidebar'
+		);
+
+		if (!$success) {
+			error_log('Failed to register Findkit block type ');
+		}
+	}
+}
