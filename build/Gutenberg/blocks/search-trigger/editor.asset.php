438801f618ec322ace54a9d53ec48262fa9e9464
Esa-Matti Suuronen <esa-matti@suuronen.org>
2023-09-21 18:16:56 +0300

    Add Search Trigger Block

---
+++ build/Gutenberg/blocks/search-trigger/editor.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '3df40594f04c725690f4');
---
+++ src/Gutenberg/blocks/search-trigger/view.tsx
@@ -0,0 +1,43 @@
+import "./view.css";
+import { FindkitUI } from "@findkit/ui";
+
+const settings: {
+	projectId?: string;
+} = (window as any).FINDKIT_SEARCH_TRIGGER_VIEW;
+
+if (!settings.projectId) {
+	throw new Error(
+		"Cannot activate Findkit public token is not defined in the settings",
+	);
+}
+
+const ui = new FindkitUI({
+	publicToken: settings.projectId,
+});
+
+function bind(el: Element) {
+	if (!(el instanceof HTMLElement)) {
+		return;
+	}
+
+	if (!(el instanceof HTMLButtonElement)) {
+		el.role = "button";
+		el.tabIndex = 0;
+	}
+
+	ui.openFrom(el);
+}
+
+function bindAll() {
+	document
+		.querySelectorAll(
+			".wp-block-findkit-search-trigger a, .wp-block-findkit-search-trigger figure",
+		)
+		.forEach(bind);
+}
+
+if (document.readyState === "loading") {
+	document.addEventListener("DOMContentLoaded", bindAll);
+} else {
+	bindAll();
+}
