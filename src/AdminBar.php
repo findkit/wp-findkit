38f331dd2edf704c9292df34a6889275a53ec15e
Esa-Matti Suuronen <esa-matti@suuronen.org>
2023-09-22 17:21:48 +0300

    Fix admin search edit link in the frontend

--- src/AdminBar.php
+++ src/AdminBar.php
@@ -16,6 +16,8 @@ class AdminBar
 		\add_action('admin_bar_menu', [$this, '__admin_bar_menu'], 100);
 		\add_action('admin_head', [$this, '__action_head'], 10);
 		\add_action('wp_head', [$this, '__action_head'], 10);
+		add_action('admin_init', [$this, '__action_handle_edit_redirect'], 10);
+		add_action('init', [$this, '__action_handle_edit_redirect'], 10);
 	}
 
 	function __action_head()
@@ -78,5 +80,50 @@ class AdminBar
 				])
 			)
 		);
+	}
+
+	function __action_handle_edit_redirect()
+	{
+		if (!\current_user_can('edit_posts')) {
+			return;
+		}
+
+		$url = filter_input(INPUT_GET, 'findkit_edit_redirect', FILTER_SANITIZE_URL);
+
+		if (empty($url)) {
+			return;
+		}
+
+		// Handle site.example/?p=123 style urls
+		$parsed = parse_url($url);
+		if (!empty($parsed['query'])) {
+			parse_str($parsed['query'], $qs);
+			if (!empty($qs['p'])) {
+				$this->redirect_to_post_id($qs['p']);
+			}
+		}
+
+		$this->redirect_to_post_id(url_to_postid($url));
+	}
+
+	function redirect_to_post_id($post_id)
+	{
+		$url = filter_input(INPUT_GET, 'findkit_edit_redirect', FILTER_SANITIZE_URL);
+		if (!get_post($post_id)) {
+			http_response_code(404);
+			printf(
+				'No post found for %s',
+				esc_url($url)
+			);
+
+			echo '<p><a onclick="history.back()" href="#">Go back</a></p>';
+
+			die();
+		}
+
+		wp_redirect(
+			get_site_url() . "/wp-admin/post.php?post=$post_id&action=edit"
+		);
+		die();
 	}
 }
--- src/JavaScriptGlobal.php
+++ src/JavaScriptGlobal.php
@@ -27,50 +27,8 @@ class JavaScriptGlobal
 			// @findkit/ui library loads
 			-10
 		);
-
-		add_action('admin_init', [$this, '__action_handle_edit_redirect']);
 	}
 
-	function __action_handle_edit_redirect()
-	{
-		$url = filter_input(INPUT_GET, 'findkit_edit_redirect', FILTER_SANITIZE_URL);
-
-		if (empty($url)) {
-			return;
-		}
-
-		// Handle site.example/?p=123 style urls
-		$parsed = parse_url($url);
-		if (!empty($parsed['query'])) {
-			parse_str($parsed['query'], $qs);
-			if (!empty($qs['p'])) {
-				$this->redirect_to_post_id($qs['p']);
-			}
-		}
-
-		$this->redirect_to_post_id(url_to_postid($url));
-	}
-
-	function redirect_to_post_id($post_id)
-	{
-		$url = filter_input(INPUT_GET, 'findkit_edit_redirect', FILTER_SANITIZE_URL);
-		if (!get_post($post_id)) {
-			http_response_code(404);
-			printf(
-				'No post found for %s',
-				esc_url($url)
-			);
-
-			echo '<p><a onclick="history.back()" href="#">Go back</a></p>';
-
-			die();
-		}
-
-		wp_redirect(
-			get_site_url() . "/wp-admin/post.php?post=$post_id&action=edit"
-		);
-		die();
-	}
 
 
 	function enable_jwt()
