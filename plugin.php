a7d0637977b46afcd8824ee55e7e1e448680bde5
Esa-Matti Suuronen <esa-matti@suuronen.org>
2023-11-07 11:14:05 +0200

    Gracefully handle broken autoload

--- plugin.php
+++ plugin.php
@@ -12,15 +12,31 @@
 // users we must detect whether the project is using a global autoloader. We
 // can do that by checking whether our autoloadable classes will autoload with
 // class_exists(). If not it means there's no global autoloader in place and
-// the user is not using composer. In that case we can safely require the
+// the user is not using composer. In that case we can try to require the
 // bundled autoloader code.
 if (!\class_exists('\Findkit\Loader')) {
-	require_once __DIR__ . '/vendor/autoload.php';
+	if (\is_readable(__DIR__ . '/vendor/autoload.php')) {
+		require_once __DIR__ . '/vendor/autoload.php';
+	}
 }
 // This way we can add the vendor/ directory to git and have the plugin "just
 // work" when it is cloned to wp-content/plugins. But be careful when checking
 // the vendor/ into git so you won't add all development dependencies too. Eg.
 // before checking it in you should always run "composer install --no-dev" first.
+
+// If the class still doesn't exist it means the autoloader files are not bundled in the plugin
+if (!\class_exists('\Findkit\Loader')) {
+	add_action('admin_notices', function () {
+		$class = 'notice notice-error';
+		$message = __(
+			'Failed to fully load the Findkit-plugin. Autoload classes not found. <a target="_blank" href="https://findk.it/wpautoload">Read more</a>.',
+			'findkit'
+		);
+
+		printf('<div class="%1$s"><p>%2$s</p></div>', $class, $message);
+	});
+	return;
+}
 
 \Findkit\Loader::instance();
 
