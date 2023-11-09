079faade4462ea2ac2d9ea512d0208ea74debce6
esamattis <action@github.com>
2023-11-09 10:41:38 +0000

    Bump version number to v0.1.13

--- plugin.php
+++ plugin.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://github.com/findkit/wp-findkit
  * Description: Findkit.com helpers
  * Author: Esa-Matti Suuronen <findkit@findkit.com>
- * Version: 0.1.12
+ * Version: 0.1.13
  */
 
 // To make this plugin work properly for both Composer users and non-composer
