4ba1ee46c1817b78a4d43bc074dd592f1da2fa17
esamattis <action@github.com>
2023-12-19 14:34:46 +0000

    Commit changes in build

--- build/search-blocks-editor.tsx.asset.php
+++ build/search-blocks-editor.tsx.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data'), 'version' => '138b8900f6ec81d8e39b');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data'), 'version' => '815d622e5f3977f56d68');
