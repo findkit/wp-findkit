0378c9624b07aba3205ba1d98593d927bc898898
esamattis <action@github.com>
2023-12-19 14:12:54 +0000

    Update version numbers to v0.5.0

--- plugin.php
+++ plugin.php
@@ -9,7 +9,7 @@ if (!defined('ABSPATH')) {
  * Plugin URI: https://www.findkit.com/wordpress/
  * Description: WordPress Plugin for Findkit Site Search. See findkit.com for details
  * Author: Findkit Team <findkit@findkit.com>
- * Version: 0.4.0
+ * Version: 0.5.0
  * License: GPLv2 or later
  */
 
