5d4b7d4e0be33f2df85e265eb5e23aae56db3e65
Lauri Saarni <lauri.saarni@gmail.com>
2025-11-18 11:16:44 +0200

    Commit changes in build

--- build/search-blocks-editor.tsx.asset.php
+++ build/search-blocks-editor.tsx.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data'), 'version' => 'ace30a93602c99b12cd3');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data'), 'version' => '28ed3103793172af38dc');
