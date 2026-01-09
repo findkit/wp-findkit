10732d4edceb9d4c66fae5a23d6da8f7618c3a7e
Lauri Saarni <lauri.saarni@gmail.com>
2026-01-09 11:40:54 +0200

    Add sanitation for $_SERVER value

--- src/CrawlerCompat.php
+++ src/CrawlerCompat.php
@@ -14,7 +14,6 @@ class CrawlerCompat
 
 	function bind()
 	{
-
 		\add_filter(
 			'do_redirect_guess_404_permalink',
 			[$this, '__filter_disable_404_guess'],
@@ -32,7 +31,9 @@ class CrawlerCompat
 
 	private function is_findkit_crawler(): bool
 	{
-		$user_agent = $_SERVER['HTTP_USER_AGENT'] ?? '';
+		$user_agent = isset($_SERVER['HTTP_USER_AGENT'])
+			? sanitize_text_field(wp_unslash($_SERVER['HTTP_USER_AGENT']))
+			: '';
 		$is_crawler = stripos($user_agent, self::CRAWLER_USER_AGENT) !== false;
 
 		return $is_crawler;
@@ -40,7 +41,6 @@ class CrawlerCompat
 
 	function __filter_disable_404_guess($do_redirect)
 	{
-
 		if ($this->is_findkit_crawler()) {
 			return false;
 		}
@@ -50,7 +50,6 @@ class CrawlerCompat
 
 	function __filter_disable_old_slug_redirect($post_id)
 	{
-
 		if ($this->is_findkit_crawler()) {
 			return 0;
 		}
